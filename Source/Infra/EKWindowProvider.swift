4e519d9611778e98ddf272d81ba6782059b29e66
Rick <119639587+Rick-ahnlabio@users.noreply.github.com>
2024-11-21 14:03:45 +0900

    Update EKWindowProvider.swift

--- Source/Infra/EKWindowProvider.swift
+++ Source/Infra/EKWindowProvider.swift
@@ -249,6 +249,7 @@ final class EKWindowProvider: EntryPresenterDelegate {
     /** Dismiss entries according to a given descriptor */
     func dismiss(_ descriptor: SwiftEntryKit.EntryDismissalDescriptor, with completion: SwiftEntryKit.DismissCompletionHandler? = nil) {
         guard let rootVC = rootVC else {
+            completion?()
             return
         }
         
