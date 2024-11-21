9a5eb93df48eda3ad2bd606412e9d5284910c81c
rick <rick@ahnlab.io>
2024-11-21 16:11:05 +0900

    chore: revert

--- Source/Infra/EKWindowProvider.swift
+++ Source/Infra/EKWindowProvider.swift
@@ -254,10 +254,7 @@ final class EKWindowProvider: EntryPresenterDelegate {
     
     /** Dismiss entries according to a given descriptor */
     func dismiss(_ descriptor: SwiftEntryKit.EntryDismissalDescriptor, with completion: SwiftEntryKit.DismissCompletionHandler? = nil) {
-        guard let rootVC = rootVC else {
-            completion?()
-            return
-        }
+        guard let rootVC else { return }
         
         switch descriptor {
         case .displayed:
